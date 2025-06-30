4b1ef09bbbeb18c54149285ac78a9e52da2f1ce6
Josh Mock <joshua.mock@elastic.co>
2025-06-30 12:56:52 -0500

    Ensure multi-arch build has a buildx builder (#114)

--- .buildkite/docker-push.sh
+++ .buildkite/docker-push.sh
@@ -6,7 +6,9 @@ version_tag=$(jq -r ".version" ./package.json)
 elastic_image="docker.elastic.co/mcp/elasticsearch:$version_tag"
 
 # build image
-docker buildx build -t "$elastic_image" --platform linux/amd64,linux/arm64 .
+docker buildx create --use --name builder
+docker buildx inspect --bootstrap
+docker buildx build -t "$elastic_image" --platform linux/amd64,linux/arm64 --builder builder .
 
 # push to docker.elastic.co
 ELASTIC_PASSWORD=$(vault read -field=password secret/ci/elastic-mcp-server-elasticsearch/devtoolsmachine)
--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@
 # SPDX-License-Identifier: Apache-2.0
 FROM cgr.dev/chainguard/wolfi-base:latest@sha256:73c232274a987eac99caee0b412cc44a992874ab4a70e48e8cc8d62babbbda27
 
-RUN apk --no-cache add nodejs npm
+RUN apk update && apk --no-cache add nodejs npm
 
 WORKDIR /app
 
