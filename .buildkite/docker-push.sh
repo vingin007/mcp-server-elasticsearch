316dc77943c9a97aa5811f891b8ab71bff554d65
Josh Mock <joshua.mock@elastic.co>
2025-06-30 15:42:05 -0500

    Push latest tag whenever a new Docker image is built (#117)

--- .buildkite/docker-push.sh
+++ .buildkite/docker-push.sh
@@ -3,15 +3,17 @@
 set -euo pipefail
 
 version_tag=$(jq -r ".version" ./package.json)
-elastic_image="docker.elastic.co/mcp/elasticsearch:$version_tag"
+elastic_image="docker.elastic.co/mcp/elasticsearch"
 
 # build image
 docker buildx create --use --name builder
 docker buildx inspect --bootstrap
-docker buildx build -t "$elastic_image" --platform linux/amd64,linux/arm64 --builder builder --load .
+docker buildx build -t "$elastic_image:$version_tag" --platform linux/amd64,linux/arm64 --builder builder --load .
+docker tag "$elastic_image:$version_tag" "$elastic_image:latest"
 
 # push to docker.elastic.co
 ELASTIC_PASSWORD=$(vault read -field=password secret/ci/elastic-mcp-server-elasticsearch/devtoolsmachine)
 docker login -u "devtoolsmachine" -p "$ELASTIC_PASSWORD" docker.elastic.co
-docker push "$elastic_image"
+docker push "$elastic_image:$version_tag"
+docker push "$elastic_image:latest"
 docker logout docker.elastic.co
